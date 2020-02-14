8ada84fde2df75d1e34b4905734eabca8b050cb7
guy.golan <guygolan91@gmail.com>
2020-02-14 15:44:24 +0200

    added scheduling

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import Server from './server/Server';
-
+import schedule from 'node-schedule';
 const port = 4000;
 const server = new Server();
+
+const fetchDataRule = '*/5 * * * *';
+const job = schedule.scheduleJob('Fetch data', fetchDataRule, () => {
+    console.log('fetching data');
+});
+
 server.start(port, () => {
     console.log(`Server started at port ${port}`);
+    job.invoke();
 });
