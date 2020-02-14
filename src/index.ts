832fc5bf92be7cddeb3f818216240e55a94a0f1e
guy.golan <guygolan91@gmail.com>
2020-02-14 18:20:43 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import Application from './server/Application';
+import express, {Application} from 'express';
+import bodyParser from 'body-parser';
 
 const port = 4000;
-const app = new Application();
+const app: Application = express();
 
-app.start(port, () => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 });
