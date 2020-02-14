ce81727b1d9021d32426fdb212820cdec9d7ec5c
guy.golan <guygolan91@gmail.com>
2020-02-14 17:44:29 +0200

    removed node-schedule

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import Application from './server/Application';
-import schedule from 'node-schedule';
+
 const port = 4000;
 const app = new Application();
 
-const fetchDataRule = '* */5 * * * *';
-const job = schedule.scheduleJob('Fetch data', fetchDataRule, () => {
-    console.log('fetching data');
-});
-
 app.start(port, () => {
     console.log(`Server started at port ${port}`);
-    job.invoke();
 });
