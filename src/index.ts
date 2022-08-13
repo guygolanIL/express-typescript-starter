4a3bb69c2d58fcf7766a050fbe8f23300399b3d5
Guy Golan <guygolan91@gmail.com>
2022-08-13 20:52:21 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, {Application, json} from 'express';
-import { hello } from './hello';
 
 const port = 4000;
 const app: Application = express();
@@ -7,6 +6,5 @@ const app: Application = express();
 app.use(json());
 
 app.listen(port, () => {
-    console.log(hello);
     console.log(`Server started at port ${port}`);
 });
