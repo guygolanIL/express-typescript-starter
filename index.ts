8782e8387f87b6dc3d2809da6c33bcf879dfaa91
Guy Golan <guygolan91@gmail.com>
2022-09-05 21:16:36 +0300

    removed gql

--- index.ts
+++ index.ts
@@ -1,18 +1,9 @@
-import express, {Application, json} from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { resolvers } from './graphql/resolvers';
-import { schema } from './graphql/schema';
+import express, { Application, json } from 'express';
 
 const port = 4000;
 const app: Application = express();
 
 app.use(json());
-
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true,
-    rootValue: resolvers,
-}));
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
